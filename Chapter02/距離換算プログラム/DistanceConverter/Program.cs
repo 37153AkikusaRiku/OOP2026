28e6188d7b672babbcd33364172a64baa51e2cf6
infosys <infosys@C4220>
2026-04-20 11:43:04 +0900

    プロジェクト ファイルを追加します。

---
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace DistanceConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            if (args.Length >= 1 && args[0] == "-tom") {
+
+
+                //フィールドからメールへの対応表を出力
+                for (int i = 0; i < 10; i++) {
+                    double meter = FeetToMeter(i+1);
+                    Console.WriteLine($"{i + 1}ft={meter:0.000}m");//$と{}で文字列の中に変数などを入れれる
+                }
+            }
+            else
+            {
+                for (int a = 0; a <=10 ; a++)
+                {
+                    double feet = MeterToFeet(a+1);
+                    Console.WriteLine($"{a + 1}ft={a:0.000}ft");
+
+                }
+            }
+        }
+
+        static double FeetToMeter(int i){
+            return i * 0.3048;
+        }
+        static double MeterToFeet(int a)
+        {
+            return a/ 0.3048;
+        }
+    }
+}
