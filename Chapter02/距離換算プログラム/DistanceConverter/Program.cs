3bd49a4789bdafbd9fe7c16688365f185c82950c
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-04-22 11:16:35 +0900

    コマンドライン引数で、出る結果の数が変わる

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -1,40 +1,112 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System.ComponentModel.Design;
+
+using System.Diagnostics.Metrics;
 
 namespace DistanceConverter
+
 {
+
     internal class Program
+
     {
-        static void Main(string[] args){
-            if (args.Length >= 1 && args[0] == "-tom") {
-                PrintFFeetToMeterList(1, 10);
+
+        public static object PrintMeterToFeet { get; private set; }
+
+        static void Main(string[] args)
+
+        {
+
+            if (args.Length >= 1 && args[0] == "-tom")
+
+            {
+
+                PrintFeetToMeterList(int.Parse(args[1]), int.Parse(args[2]));
+
+            }//メートルへの変換
+
+            else if (args.Length >= 1 && args[0] == "-tof")
+
+            {
+
+                PrintMeterToFeetList(int.Parse(args[1]), int.Parse(args[2]));//メートルからフィートへの変換
+
             }
+
+
             else
+
             {
-                for (int a = 0; a <=10 ; a++)
-                {
-                    double feet = MeterToFeet(a+1);
-                    Console.WriteLine($"{a + 1}ft={a:0.000}ft");
 
-                }
+                Console.WriteLine("因数エラー");
+                Error(args.Length);
             }
+
         }
 
-        static void PrintFFeetToMeterList(int start, int stop)
+
+        private static void PrintMeterToFeetList(int start, int stop)
+
         {
-            //フィールドからメールへの対応表を出力
-            for (int i = 0; i < 10; i++)
+
+            for (int meter = start; meter <= stop; meter++)
+
             {
-                double meter = FeetToMeter(i + 1);
-                Console.WriteLine($"{i + 1}ft={meter:0.000}m");//$と{}で文字列の中に変数などを入れれる
+
+                double feet = MeterToFeet(meter);
+
+                Console.WriteLine($"{meter}m={feet:0.0000}ft");
+
             }
+
         }
 
-        static double FeetToMeter(int i){
-            return i * 0.3048;
-        }
-        static double MeterToFeet(int a)
+        private static void PrintFeetToMeterList(int start, int stop)
+
         {
-            return a/ 0.3048;
+
+            for (int feet = start; feet <= stop; feet++)
+
+            {
+
+                double meter = FeetToMeter(feet);
+
+                Console.WriteLine($"{feet}ft={meter:0.0000}m");
+
+            }
+
         }
+
+        static double FeetToMeter(int feet)//フィートからメートルを求める
+
+        {
+
+            //feet*0.3048
+
+            return feet * 0.3048;
+
+        }
+
+        //メートルからフィートを求める
+
+        static double MeterToFeet(int meter)
+
+        {
+
+            //feet*0.3048
+
+            return meter / 0.3048;
+
+        }
+        static void Error(int cnt)
+        {
+            if (cnt != 3) Console.WriteLine("引数が3つではありません");
+
+
+        }
+
+
     }
+
 }
+
+
