32f5bb395ae206b4cfe9db201858347eb4098cf3
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-04-22 09:21:35 +0900

    機能追加

--- Chapter02/距離換算プログラム/DistanceConverter/Program.cs
+++ Chapter02/距離換算プログラム/DistanceConverter/Program.cs
@@ -6,13 +6,7 @@ namespace DistanceConverter
     {
         static void Main(string[] args){
             if (args.Length >= 1 && args[0] == "-tom") {
-
-
-                //フィールドからメールへの対応表を出力
-                for (int i = 0; i < 10; i++) {
-                    double meter = FeetToMeter(i+1);
-                    Console.WriteLine($"{i + 1}ft={meter:0.000}m");//$と{}で文字列の中に変数などを入れれる
-                }
+                PrintFFeetToMeterList(1, 10);
             }
             else
             {
@@ -22,6 +16,16 @@ namespace DistanceConverter
                     Console.WriteLine($"{a + 1}ft={a:0.000}ft");
 
                 }
+            }
+        }
+
+        static void PrintFFeetToMeterList(int start, int stop)
+        {
+            //フィールドからメールへの対応表を出力
+            for (int i = 0; i < 10; i++)
+            {
+                double meter = FeetToMeter(i + 1);
+                Console.WriteLine($"{i + 1}ft={meter:0.000}m");//$と{}で文字列の中に変数などを入れれる
             }
         }
 
