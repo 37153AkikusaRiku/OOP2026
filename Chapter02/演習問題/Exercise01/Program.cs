6a714d98eec6edeb9b00399fd736723703e03be4
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-05-13 10:21:22 +0900

    データ入力対応

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -3,24 +3,36 @@ namespace Exercise01 {
     public class Program {
         public static void Main(string[] args) {
             Console.WriteLine("Hello, World!");
+            var songs = new List<Song>();
+            //PrintIn();
 
-            var songs = new Song[] {
+            while (true) {
 
-                new Song("Let it be", "The Beatles", 243),
-                new Song("Bridge Over Troubled Water", "Simon & Garfunkel", 293),
-                new Song("Close To You", "Carpenters", 276),
-                new Song("Honesty", "Billy Joel", 231),
-                new Song("I Will Always Love You", "Whitney Houston", 273),
 
-            };
+                Console.Write("曲名");
+                string? title = Console.ReadLine();
 
+                if (title == "end") { break; } else {
+
+                    Console.Write("アーティスト名");
+                    string? artistname = Console.ReadLine();
+
+                    Console.Write("演奏時間");
+                    int lenght = int.Parse(Console.ReadLine());
+
+                    Song song = new Song(title, artistname, lenght);
+
+                    songs.Add(song);
+                }
+            }
             PrintSongs(songs);
         }
 
-        public static void PrintSongs(Song[] songs) {
-            foreach(var Song in songs) {
-                Song[]=
-                Console.WriteLine($"{Song.Title}{Song.ArtistName}{Song.Lenght}");
+
+        public static void PrintSongs(IEnumerable<Song> songs) {
+            foreach (var Song in songs) {
+
+                Console.WriteLine($"{Song.Title}{Song.ArtistName}{Song.Lenght / 60}:{Song.Lenght % 60:00}");
             }
         }
     }
