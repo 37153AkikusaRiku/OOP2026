785e1c6f6ab61c6706daa18223abb03a7f0b5917
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-05-13 11:17:16 +0900

    未完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    internal class InchConverter {
+
+        public static double Convert(int con,double tei){
+            return con * tei;
+            }
+
+
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            double tei = .0254;//定数
+            InchConverter inchConverter = new InchConverter();
+
+            for (int con=1; con < 11; con++) {
+                InchConverter.Convert(con, tei);
+            }
+           
+            }
+
+
+
+        }
+    }
+}
