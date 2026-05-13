07c4804953b8cf089312df414975f63d01795a6d
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-05-13 11:33:13 +0900

    インチをメートルに

--- Chapter02/演習問題/Exercise02/InchConverter.cs
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -7,9 +7,33 @@ using System.Threading.Tasks;
 namespace Exercise02 {
     internal class InchConverter {
 
-        public static double Convert(int con,double tei){
-            return con * tei;
-            }
+        private static readonly double ratio = 0.0254;//定数
+        public static double FromMeter(int meter)
+            //メートルからインチを求める
+        {
+            //feet*0.3048
+            return meter / ratio;
+        }
+
+
+        public static double ToMeter(int feet)
+            //インチからメートルを求める
+        {
+            return feet * ratio;
+        }
+
+
+
+
+
+
+
+
+
+
+        //public static double Convert(int con,double tei){
+        //    return con * tei;
+        //    }
 
 
     }
--- Chapter02/演習問題/Exercise02/Program.cs
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -1,17 +1,19 @@
 ﻿namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
-            double tei = .0254;//定数
-            InchConverter inchConverter = new InchConverter();
-
-            for (int con=1; con < 11; con++) {
-                InchConverter.Convert(con, tei);
-            }
+            PrintFeetToMeterList(1, 10);
            
+        }
+        private static void PrintFeetToMeterList(int start, int stop) {
+            //FeetConverter converter = new FeetConverter();
+            for (int feet = start; feet <= stop; feet++) {
+                double meter = InchConverter.ToMeter(feet);
+                Console.WriteLine($"{feet}inch={meter:0.0000}m");
             }
 
+        }
 
 
         }
+
     }
-}
