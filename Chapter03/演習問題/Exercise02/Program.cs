7e03b197437f4591cbaa03b93ece395bb1744259
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-05-21 11:19:46 +0900

    3-2-1,2,3,4完成

---
+++ Chapter03/演習問題/Exercise02/Program.cs
@@ -0,0 +1,58 @@
+﻿
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var cities = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            Console.WriteLine("***** 3.2.1 *****");
+            Exercise2_1(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.2 *****");
+            Exercise2_2(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.3 *****");
+            Exercise2_3(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.4 *****");
+            Exercise2_4(cities);
+            Console.WriteLine();
+        }
+
+        private static void Exercise2_1(List<string> cities) {
+            var name = Console.ReadLine();
+            var findindex = cities.FindIndex(s => s == name);
+            Console.WriteLine(findindex);
+
+	        
+        }
+
+        private static void Exercise2_2(List<string> cities) {
+            var count = cities.Count(c => c.Contains('o'));
+            Console.WriteLine(count);
+	        
+        }
+
+        private static void Exercise2_3(List<string> cities) {
+            var where = cities.Where(c => c.Contains('o'));
+            foreach (var item in where) {
+                Console.WriteLine(item);
+            }
+
+        }
+
+        private static void Exercise2_4(List<string> cities) {
+            var where = cities.Where(c => c.Contains('B'));
+            foreach (var item in where) {
+
+                Console.WriteLine(item.Length);
+
+            }
+        }
+    }
+}
