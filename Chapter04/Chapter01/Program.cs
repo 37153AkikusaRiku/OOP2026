9880e7c62b0c132d2d4b59cb336a4aa7d95f882b
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-05-27 11:20:08 +0900

    4-1-2完成

--- Chapter04/Chapter01/Program.cs
+++ Chapter04/Chapter01/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Security.Cryptography;
+
 namespace Chapter01 {
     internal class Program {
         static void Main(string[] args) {
@@ -17,7 +19,31 @@ namespace Chapter01 {
 
         private static void Exercise1(List<string> langs) {
 
-            //var select = langs.Select(a => a = langs.Where(c => c.Contains('o'));
+            foreach(var lang in langs) {
+                if (lang.Contains('S')) {
+                    Console.WriteLine(lang);
+                }
+            }
+
+            for (int i = 0; i < langs.Count; i++) {
+                if (langs[i].Contains('S')) {
+                    Console.WriteLine(langs[i]);
+                }
+            }
+
+
+            int count = 0;
+            while(count < langs.Count) {
+                if(langs[count].Contains('S'))
+                    Console.WriteLine(langs[count]);
+            }
+
+
+           
+
+        }
+
+        private static void Exercise2(List<string> langs) {
 
             var where = langs.Where(c => c.Contains('S')).ToList();
 
@@ -28,7 +54,7 @@ namespace Chapter01 {
             }
             //for文
             Console.WriteLine("\nfor文で出力");
-            
+
             for (int i = 0; i < where.Count(); i++) {
 
 
@@ -39,20 +65,21 @@ namespace Chapter01 {
             Console.WriteLine("\nwhile文で出力");
             int a = 0;
             while (true) {
-                
+
                 Console.WriteLine(where[a]);
-                a = a+1;
+                a = a + 1;
                 if (a == where.Count()) { break; }
 
             }
 
-        }
 
-        private static void Exercise2(List<string> langs) {
-            
+
+
         }
 
         private static void Exercise3(List<string> langs) {
+
+
 
         }
     }
