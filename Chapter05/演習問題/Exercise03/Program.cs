ee768fbe6d8cd76cfe4f9e0f3183192f1938545e
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-06-08 10:54:46 +0900

    recordとクラスの違い

---
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Exercise03 {
+    //クラスの定義（ToString()を定義していない）
+    internal class Program {
+        static void Main(string[] args) {
+            var ym1 = new YearMonth(2002, 10);
+            var ym2 = new YearMonth(2002, 10);
+
+            if (ym1==ym2) {
+                Console.WriteLine("等しい");
+            } else { Console.WriteLine("等しくない"); }
+
+        }
+
+
+    }
+}
