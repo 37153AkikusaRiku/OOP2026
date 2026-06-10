f660c787820f017269e4ff4b9eb4e2de72d6f4e1
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-06-10 10:30:21 +0900

    6-1,2完成

---
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var box = Console.ReadLine();
+            var changeint = int.Parse(box);
+            string changestr = changeint.ToString("#,0");
+
+            Console.WriteLine(changestr);
+        }
+    }
+}
