82f2f30f8a5a605e849b90fb00c44a82f3dbd54c
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-06-19 10:34:18 +0900

    7-1-1,2,3,4,5完成

---
+++ chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+
+
+            Console.WriteLine("7.1.1");
+            Exercise1(numbers);
+
+            Console.WriteLine("7.1.2");
+            Exercise2(numbers);
+
+            Console.WriteLine("7.1.3");
+            Exercise3(numbers);
+
+            Console.WriteLine("7.1.4");
+            Exercise4(numbers);
+
+            Console.WriteLine("7.1.5");
+            Exercise5(numbers);
+        }
+
+        private static void Exercise1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise2(int[] numbers) {
+            Console.WriteLine(numbers[numbers.Length - 1] + numbers[numbers.Length - 2]);
+        }
+
+        private static void Exercise3(int[] numbers) {
+            var numbersEx = numbers.Select(n=> n.ToString("000"));
+            foreach (var item in numbersEx) {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Exercise4(int[] numbers) {
+            var numbersMin = numbers.OrderBy(n => n).Take(3);
+            foreach (var item in numbersMin) {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void Exercise5(int[] numbers) {
+            var numbersUni = numbers.Distinct().Where(n => n >= 10);
+            foreach (var item in numbersUni) {
+                Console.WriteLine(item);
+
+            }
+        }
+    }
+}
