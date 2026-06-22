e1d2c0b0514b7b0ef0bf54b7939dced552a289ee
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-06-22 11:24:00 +0900

    設問１_Studentクラス完成

--- TestProject/Test01_01/Student.cs
+++ TestProject/Test01_01/Student.cs
@@ -1,6 +1,8 @@
 ﻿namespace Test01_01 {
     public class Student {
-        
+        public string Name { get; private set; } = null!;
+        public string Subject { get; private set; } = null!;
+        public int Score { get; private set; }=0;
 
 
     }
