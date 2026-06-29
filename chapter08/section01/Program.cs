f7b96fe84816c330ecc4599d167be24bfbb11918
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-06-29 14:02:12 +0900

    mesoddo完成

--- chapter08/section01/Program.cs
+++ chapter08/section01/Program.cs
@@ -23,69 +23,78 @@
             foreach (var item in prefOfficeDict) {
                 Console.WriteLine(item.Key + item.Value);
             }
-            Console.WriteLine("＊＊＊メニュー＊＊＊");
-            Console.WriteLine("1：一覧表示　2：検索　9：終了");
 
 
 
 
 
-            int result = int.Parse(Console.ReadLine());
-           
+
+
+
+
+            while (true) {
+                Console.WriteLine("＊＊＊メニュー＊＊＊");
+                Console.WriteLine("1：一覧表示　2：検索　9：終了");
+                int result = int.Parse(Console.ReadLine());
                 switch (result) {
-                    case 1:
-                        foreach (var item in prefOfficeDict) {
-                            Console.WriteLine(item.Key + item.Value);
-                            
-                        }
-                        break;
-                    case 2:
-                        Console.Write("検索：");
-                        var ch = Console.ReadLine();
-                        foreach (var item in prefOfficeDict) {
-                            if (item.Key == ch) {
-                                Console.WriteLine(item);
-                                break;
-                            }
-                            Console.WriteLine("ぶきないよっ！");
-                        }
 
+                    case 1:
+                        vaw(prefOfficeDict);
                         break;
-                    case 9:break;
+                    //foreach (var item in prefOfficeDict) {
+                    //    Console.WriteLine(item.Key + item.Value);
+                    //}
+                    //break;
+
+                    case 2:
+                        findVaw(prefOfficeDict);
+                        break;
+                        //Console.Write("検索：");
+                        //var ch = Console.ReadLine();
+                        //foreach (var item in prefOfficeDict) {
+                        //    if (item.Key == ch) {
+                        //        Console.WriteLine(item);
+                        //        break;
+                        //    }
+                        //    Console.WriteLine("ぶきないよっ！");
+                        //}
+                        //break;
+
+                    case 9:
+                        break;
                 }
 
-            
-            //1 => vaw(prefOfficeDict)
-            //    2 =>findVaw(prefOfficeDict),
-            //    3=>"終了",
-            //    _ => "erarr"
-            //}; 
+            }
 
 
 
 
 
-            //static string vaw(Dictionary<string, string> prefOfficeDict) {
-            //    foreach (var item in prefOfficeDict) {
-            //        return "都道府県" + item.Key + "県庁" + item.Value;
-
-            //    }
-            //    return "終了";
-            //}
-
-            //static string findVaw(Dictionary<string, string> prefOfficeDict) {
-            //    var ch = Console.ReadLine();
-            //    foreach (var item in prefOfficeDict) {
-            //        if (item.Key == ch) {
-            //             Console.WriteLine(item);
-            //            return "";
-            //        }
 
 
-            //    }
-            //    return "";
-            //}
+            static void vaw(Dictionary<string, string> prefOfficeDict) {
+                foreach (var item in prefOfficeDict) {
+                    Console.WriteLine("都道府県:" + item.Key + "県庁:" + item.Value);
 
+                }
+            }
+
+            static void findVaw(Dictionary<string, string> prefOfficeDict) {
+                Console.Write("検索：");
+                var ch = Console.ReadLine();
+                
+                
+
+                foreach (var item in prefOfficeDict) {
+                    if (item.Key == ch) {
+                        Console.WriteLine("都道府県:" + item.Key + "県庁:" + item.Value);
+                        break;
+                    }
+                    
+                }
+            }
         }
     }
 }
+    
+
