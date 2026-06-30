e3950b42250456827642afd6abcdf9bce1d5342f
理陸 秋草 <oibc37153@ojs.ac.jp>
2026-06-30 14:52:04 +0900

    8-*1-1完成

---
+++ chapter08/Section02/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Section02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.TxT");
+            var we = new WordsExtractor(lines);
+            foreach (var word in we.Extract()) {
+                Console.WriteLine(word);
+
+            }
+
+
+        }
+    }
+}
